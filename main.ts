69baf0ef80be6b33bfe64f258e6a3448a8eed3e2
samFRcouldnttinkofausername <122544219+samFRcouldnttinkofausername@users.noreply.github.com>
2023-01-31 13:03:05 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,26 @@
 function updateScorces () {
+    let p2 = 0
     OLED.clear()
     OLED.writeStringNewLine("Player 1: " + p1)
-    OLED.writeStringNewLine("Player 2: " + p1)
-    OLED.writeStringNewLine("Ties: " + p1)
-    OLED.writeStringNewLine("Rounds: " + p1)
+    OLED.writeStringNewLine("Player 2: " + p2)
+    OLED.writeStringNewLine("Ties: " + ties)
+    OLED.writeStringNewLine("Rounds: " + rounds)
 }
-let p1 = 0
-OLED.init(128, 64)
-OLED.writeStringNewLine("Wanna play a game? Its gonna be sooo funny")
-p1 = 0
-p1 = 0
+input.onGesture(Gesture.Shake, function () {
+    reset()
+})
+function reset () {
+    OLED.init(128, 64)
+    OLED.writeStringNewLine("Wanna play a game? It would be sooo funny")
+    p1 = 0
+    p1 = 0
+    rounds = 0
+    ties = 0
+    basic.pause(2000)
+    OLED.clear()
+    updateScorces()
+}
 let rounds = 0
 let ties = 0
-basic.pause(2000)
-OLED.clear()
-updateScorces()
+let p1 = 0
+reset()
